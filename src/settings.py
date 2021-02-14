b9c5e95e9e874c51ca75555555233954081e05e3
devsetgo <mikeryan56@gmail.com>
2021-02-13 21:04:55 -0500

    formatting

--- src/settings.py
+++ src/settings.py
@@ -19,7 +19,7 @@ from pydantic import AnyUrl, BaseSettings, EmailStr
 
 class Settings(BaseSettings):
     # use_env = "dotenv"
-    
+
     app_version: str = "1.0.0"
     release_env: str = "prd"
     https_on: bool = False
@@ -30,39 +30,39 @@ class Settings(BaseSettings):
     workers: int = 2
     csrf_secret = secrets.token_urlsafe(64)
     secret_key = secrets.token_hex(64)
-    invalid_character_list:list = [
-    " ",
-    ";",
-    "<",
-    ">",
-    "/",
-    "\\",
-    "{",
-    "}",
-    "[",
-    "]",
-    "+",
-    "=",
-    "?",
-    "&",
-    "," ":",
-    "'",
-    ".",
-    '"',
-    "`",
-]
+    invalid_character_list: list = [
+        " ",
+        ";",
+        "<",
+        ">",
+        "/",
+        "\\",
+        "{",
+        "}",
+        "[",
+        "]",
+        "+",
+        "=",
+        "?",
+        "&",
+        "," ":",
+        "'",
+        ".",
+        '"',
+        "`",
+    ]
 
     loguru_retention: str = "10 days"
     loguru_rotation: str = "100 MB"
     loguru_logging_level: str = "INFO"
-    release_env:str = "prd"
-    debug:bool=False
-    sendgrid_key:str = "insert-key"
-    login_timeout:int=120
-    admin_create:bool=False
-    admin_user_name:str=None
-    admin_user_key:str=None
-    admin_user_email:str=None
+    release_env: str = "prd"
+    debug: bool = False
+    sendgrid_key: str = "insert-key"
+    login_timeout: int = 120
+    admin_create: bool = False
+    admin_user_name: str = None
+    admin_user_key: str = None
+    admin_user_email: str = None
 
     class Config:
         env_file = ".env"
