633428b76a1725830b4f773dbcff086ac576764d
devsetgo <mikeryan56@gmail.com>
2021-06-12 20:09:30 -0400

    stop logging favicon

--- src/core/logger_middleware.py
+++ src/core/logger_middleware.py
@@ -7,13 +7,15 @@ from starlette.middleware.base import BaseHTTPMiddleware
 class LoggerMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
         response = await call_next(request)
-        logger.debug(f"request data: {dict(request)}")
+        
         method = request.method
         url = request.url
         client = request.client.host
         # print(type(url), type(client))
-        logger.info(
-            f"Request Method: {method.upper()} request via {url} accessed from {client}"
-        )
-        # response.headers['Custom'] = 'Example'
+
+        if "favicon.ico" not in str(url):
+            logger.info(
+                f"Request Method: {method.upper()} request via {url} accessed from {client}"
+            )
+            logger.debug(f"full_request_data: {dict(request)}")
         return response
